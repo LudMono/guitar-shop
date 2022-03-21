530ee441add66f96a35696c3035a7bc4a26c6d6c
LudMono <theblamno@gmail.com>
2022-03-21 17:28:08 +0300

    feat: add catalog products

---
+++ constants/catalog.js
@@ -0,0 +1,58 @@
+const CATALOG = [
+  {
+    id: 1,
+    name: 'Yamaha ERG121U',
+    img: 'https://ludmono.github.io/guitar-shop/img/catalog/1.jpg',
+    price: 14500
+  },
+  {
+    id: 2,
+    name: 'Yamaha Pacifica012',
+    img: 'https://ludmono.github.io/guitar-shop/img/catalog/2.jpg',
+    price: 18900
+  },
+  {
+    id: 3,
+    name: 'SQUIER By FENDER MM STRAT',
+    img: 'https://ludmono.github.io/guitar-shop/img/catalog/3.jpg',
+    price: 28100
+  },
+  {
+    id: 4,
+    name: 'Washburn SB15PTS',
+    img: 'https://ludmono.github.io/guitar-shop/img/catalog/4.jpg',
+    price: 18100
+  },
+  {
+    id: 5,
+    name: 'Cort GB34J BK',
+    img: 'https://ludmono.github.io/guitar-shop/img/catalog/5.jpg',
+    price: 18100
+  },
+  {
+    id: 6,
+    name: 'Ibanez GSR180 BK',
+    img: 'https://ludmono.github.io/guitar-shop/img/catalog/6.jpg',
+    price: 22500
+  },
+  {
+    id: 7,
+    name: 'Fender CD-60S ALL',
+    img: 'https://ludmono.github.io/guitar-shop/img/catalog/7.jpg',
+    price: 42100
+  },
+  {
+    id: 8,
+    name: 'Cort AD810',
+    img: 'https://ludmono.github.io/guitar-shop/img/catalog/8.jpg',
+    price: 11600
+  },
+  {
+    id: 9,
+    name: 'Cort SFX-ME',
+    img: 'https://ludmono.github.io/guitar-shop/img/catalog/9.jpg',
+    price: 14600
+  },
+
+
+]
