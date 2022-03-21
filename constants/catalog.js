9d7a886681add4965d523b2af2f6bf0957d46fdb
LudMono <theblamno@gmail.com>
2022-03-21 17:28:08 +0300

    feat: add catalog products

---
+++ constants/catalog.js
@@ -0,0 +1,21 @@
+const CATALOG = [
+  {
+    id: 1,
+    name: 'Yamaha ERG121U',
+    img: 'img/catalog/1.jpg',
+    price: 14500
+  },
+  {
+    id: 2,
+    name: 'Yamaha Pacifica012',
+    img: 'img/catalog/2.jpg',
+    price: 18900
+  },
+  {
+    id: 3,
+    name: 'SQUIER By FENDER MM STRAT',
+    img: 'img/catalog/3.jpg',
+    price: 28100
+  },
+
+]
