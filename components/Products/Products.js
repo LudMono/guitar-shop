d530e5ebd84380424f545392e6ec4beb4ab028a4
LudMono <theblamno@gmail.com>
2022-03-22 09:58:42 +0300

    feat: add purchase state

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,6 +1,38 @@
 class Products {
+  constructor() {
+    this.activeClassName = "button--active";
+    this.addLabel = "Добавить в корзину";
+    this.removeLabel = "Удалить из корзины";
+  }
+
+  handlePurchase(el, id) {
+    const { isExist, products } = localStorageProducts.setProducts(id);
+
+    if (isExist) {
+      el.classList.add(this.activeClassName);
+      el.innerText = this.removeLabel;
+    } else {
+      el.classList.remove(this.activeClassName);
+      el.innerText = this.addLabel;
+    }
+  }
+
   render() {
+    const productsStore = localStorageProducts.getProducts();
+
     const items = CATALOG.map(({ id, name, img, price }) => {
+      id = id.toString()
+
+      let activeState = "";
+      let buttonLabel = "";
+
+      if (productsStore.indexOf(id) === -1) {
+        buttonLabel = this.addLabel;
+      } else {
+        buttonLabel = this.removeLabel;
+        activeState = this.activeClassName;
+      }
+
       return `
       <li class="products__item">
         <article class="item-card">
@@ -8,8 +40,11 @@ class Products {
           <div class="item-card__img">
             <img src="${img}" alt="${name}" />
           </div>
-          <h5 class="item-card__price">🚀${price.toLocaleString('ru-RU', { style: 'currency', currency: 'RUB' })}</h5>
-          <button class="button button--primary" type="button">Купить сейчас</button>
+          <h5 class="item-card__price">🚀${price.toLocaleString("ru-RU", {
+            style: "currency",
+            currency: "RUB",
+          })}</h5>
+          <button class="button button--primary ${activeState}" type="button" onclick="products.handlePurchase(this, ${id})">${buttonLabel}</button>
         </article>
       </li>
       `;
@@ -17,7 +52,7 @@ class Products {
 
     const html = `
       <ul class="products__list">
-        ${items.join('\n')}
+        ${items.join("\n")}
       </ul>
     `;
 
@@ -26,4 +61,4 @@ class Products {
 }
 
 const products = new Products();
-products.render()
+products.render();
